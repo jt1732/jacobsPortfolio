acec521f587bba883b53804f04b030deb41fe557
jt1732 <jtur305@aucklanduni.ac.nz>
2025-08-13 21:51:55 +1200

    Added grid layout and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-        <div></div>
+        <div className={'bg-gradient-to-r from-pink-200/20 to-rose-300/50 overflow-hidden'}>
+            <div className="w-screen h-10 bg-white/50 flex items-center justify-between px-5">
+
+            </div>
+            <div className={'h-screen w-screen grid grid-cols-3 grid-rows-2'}>
+            </div>
+        </div>
   );
 }
